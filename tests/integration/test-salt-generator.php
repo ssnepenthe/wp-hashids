edfa0d6a9bdd40ec97c6ce6a5f9c1d6bc59e2308
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-05-17 22:14:03 -0700

    Move salt generator test to integration suite:
    Salt generato class depends on random_int() which was introduced
    in PHP7. To run the tests for PHP5.6 WordPress must be loaded in
    order to include the random compat library

--- tests/integration/test-salt-generator.php
+++ tests/integration/test-salt-generator.php
@@ -1,6 +1,6 @@
 <?php
 
-class Salt_Generator_Test extends PHPUnit_Framework_TestCase {
+class Salt_Generator_Test extends WP_UnitTestCase {
 	/** @test */
 	function it_can_generate_a_salt_of_requested_length() {
 		$salt_gen = new WP_Hashids\Salt_Generator;
